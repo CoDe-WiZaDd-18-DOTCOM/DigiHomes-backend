143031434c2a1e1d00b08788d644fc1a9984b908
Codewizard-18 <jaswanthm811@gmail.com>
2025-03-14 10:34:56 +0530

    iniitialization

---
+++ src/test/java/com/example/DigiHomes/DigiHomesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DigiHomes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiHomesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
